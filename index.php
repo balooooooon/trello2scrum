97b281414999c8ee1a12a7f4481bd71bc2478fb4
Márius Rak <Mrius Rak>
2016-11-17 04:15:30 +0100

    GUI Done

--- index.php
+++ index.php
@@ -9,9 +9,19 @@ and open the template in the editor.
                 <meta charset="UTF-8">
                 <title></title>
         </head>
-        <body>
-                <?php
-                // put your code here
-                ?>
+        <body style="font-family:sans-serif;">
+                <form action="" method="post" enctype="multipart/form-data" style="width:450px;height:400px;position:fixed;top:0;bottom:0;left:0;right:0;margin:auto;text-align:center;line-height:1.5;font-size:1.3em">
+                        <h1 style="line-height:1.2">Nástroj na konverziu trello JSONu do PDF</h1>
+                        <label>
+                                <input type="radio" name="type" value="sprint"> Sprint backlog
+                        </label>
+                        <label>
+                                <input type="radio" name="type" value="product"> Product backlog
+                        </label>
+                        <br><br>
+                        <input type="file" name="json" style="font-size:1.2em;">
+                        <br><br>
+                        <input type="submit" value="Konvertovať JSON do PDF »" style="font-size:1.2em;padding:10px 20px">
+                </form>
         </body>
 </html>
